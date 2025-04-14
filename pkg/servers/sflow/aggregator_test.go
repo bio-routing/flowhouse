191adb31dcb8cfa64ca433a33f18bdd39e756143
Marcus Weiner <marcus.weiner@deepl.com>
2025-04-14 16:59:42 +0200

    Allow empty flush in the beginning

--- pkg/servers/sflow/aggregator_test.go
+++ pkg/servers/sflow/aggregator_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/bio-routing/bio-rd/net"
 	"github.com/bio-routing/flowhouse/pkg/models/flow"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func exampleFlow(t testing.TB, ts time.Time) *flow.Flow {
@@ -25,24 +26,48 @@ func exampleFlow(t testing.TB, ts time.Time) *flow.Flow {
 }
 
 func TestAggregatorBuffering(t *testing.T) {
-	mockedTime := time.Now()
+	initialTime := time.Now()
+	mockedTime := initialTime
 
 	out := make(chan []*flow.Flow, 10)
 	agg := newAggregator(out)
 	agg.timeNow = func() time.Time { return mockedTime }
 
 	agg.ingest(exampleFlow(t, mockedTime))
-	assert.Len(t, out, 0) // should not have flushed
+	// should have flushed an empty list at the beginning
+	select {
+	case flows := <-out:
+		assert.Empty(t, flows)
+	default:
+		t.Error("no flows in channel")
+	}
 
-	// advance time by 2 seconds
-	mockedTime = mockedTime.Add(2 * time.Second)
+	// advance time by 5 seconds
+	mockedTime = mockedTime.Add(5 * time.Second)
 
 	agg.ingest(exampleFlow(t, mockedTime))
 	assert.Len(t, out, 0) // should not have flushed
 
 	// advance time by 10 seconds
+	mockedTime = mockedTime.Add(10 * time.Second)
+
 	agg.ingest(exampleFlow(t, mockedTime))
-	assert.Len(t, out, 1) // should have flushed one record
+	assert.Len(t, out, 1) // should have flushed once
+
+	// check flushed flows
+	{
+		select {
+		case flows := <-out:
+			require.Len(t, flows, 1)
+			fl := flows[0]
+			assert.Equal(t,
+				initialTime.Add(15*time.Second).Truncate(10*time.Second).Unix(),
+				fl.Timestamp,
+			)
+		default:
+			t.Error("no flow available")
+		}
+	}
 }
 
 func must[T any](t testing.TB) func(res T, err error) T {
